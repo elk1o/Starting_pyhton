cc2227b09ace04e464d9d0c53802955e5a67dbd8
elk1o <elk1o.dev@gmail.com>
2016-03-21 09:59:44 +0100

    Create Exercices 1 workspace & Ej1

---
+++ Ejercicios_python/Ej1.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# 1)Crear una tupla de nombre productos que contenga 4 productos inventados y que a
+# continuación muestre un listado utilizando un bucle for enumerado con formato.
+
+products = ('Tv', 'Radio' ,'Microondas','Batidora')
+print('--*** Products ***--')
+for x in products:
+    print('-> %s \n' % x)
